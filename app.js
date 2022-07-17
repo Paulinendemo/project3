904258c481fdcf67937922ac9a271eefc947260c
Paulinendemo <pauline.ndemo@student.moringaschool.com>
2022-07-17 11:11:22 +0300

    updated app.js

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-function produceResults(){
+function produceReport(){
 
     event.preventDefault();
 
     let data = [];
 
-     data[0] = data["shed_A"] = parseInt(document.getElementById("shed_A").value);
-     data[1] = data["shed_B"] = parseInt(document.getElementById("shed_B").value);
-     data[2] = data["shed_C"] = parseInt(document.getElementById("shed_C").value);
-     data[3] = data["shed_D"] = parseInt(document.getElementById("shed_D").value);
+    data[0] = data["shed_A"] = parseInt(document.getElementById("shed_A").value);
+    data[1] = data["shed_B"] = parseInt(document.getElementById("shed_B").value);
+    data[2] = data["shed_C"] = parseInt(document.getElementById("shed_C").value);
+    data[3] = data["shed_D"] = parseInt(document.getElementById("shed_D").value);
 
     let sumData = data.shed_A + data.shed_B + data.shed_C + data.shed_D;
 
     document.getElementById("outputData").innerHTML += "<Br>"
+    
 
     document.getElementById("outputData").innerHTML += "<p>Your production in shed A is " + data.shed_A + " litres per day</p>";
     document.getElementById("outputData").innerHTML += "<p>Your production in shed B is " + data.shed_B + " litres per day</p>";
@@ -20,31 +21,37 @@ function produceResults(){
 
     document.getElementById("outputData").innerHTML += "<p>Your total production per day is " + sumData + " litres per day</p>";
     
+
     dailyIncome = sumData * 45;
 
     document.getElementById("outputData").innerHTML += "<Br>"
+
     document.getElementById("outputData").innerHTML += "<hr>"
+
     document.getElementById("outputData").innerHTML += "<p>Your total daily income is Ksh. " + dailyIncome + " </p>";
+
     document.getElementById("outputData").innerHTML += "<hr>"
 
     weeklyIncome = dailyIncome * 7;
 
     document.getElementById("outputData").innerHTML += "<p>Your total weekly income is Ksh. " + weeklyIncome + " </p>";
+
     document.getElementById("outputData").innerHTML += "<hr>"
+
     document.getElementById("outputData").innerHTML += "<Br>"
 
-   incomeJanuary=dailyIncome*31;
-   incomeFebruary=dailyIncome*29;
-   incomeMarch=dailyIncome*31;
-   incomeApril=dailyIncome*30;
-   incomeMay=dailyIncome*31;
-   incomeJune=dailyIncome*30;
-   incomeJuly=dailyIncome*31;
-   incomeAugust=dailyIncome*31;
-   incomeSeptember=dailyIncome*30;
-   incomeOctober=dailyIncome*31;
-   incomeNovember=dailyIncome*30;
-   incomeDecember=dailyIncome*31;
+    incomeJanuary = dailyIncome * 31;
+    incomeFebruary = dailyIncome * 29;
+    incomeMarch = dailyIncome * 31;
+    incomeApril = dailyIncome * 30;
+    incomeMay = dailyIncome * 31;
+    incomeJune = dailyIncome * 30;
+    incomeJuly = dailyIncome * 31;
+    incomeAugust = dailyIncome * 31;
+    incomeSeptember = dailyIncome * 30;
+    incomeOctober = dailyIncome * 31;
+    incomeNovember = dailyIncome * 30;
+    incomeDecember = dailyIncome * 31;
 
      
     document.getElementById("outputData").innerHTML += "<p> Your total income for January is Ksh. " + incomeJanuary + " </p>"
@@ -71,6 +78,7 @@ function produceResults(){
     document.getElementById("outputData").innerHTML += "<hr>"
 
     document.getElementById("outputData").innerHTML += "<Br>"
+
 
     // OPTIONAL TRIAL
 
@@ -144,5 +152,6 @@ function resetPage(){
 
     document.getElementById("outputData").remove("innerHTML");
     location.reload();
+
 }
 
